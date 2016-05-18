e998b55b33b4702125d4077f1ef850bd0adeb7c9
Millioner <millioner.bbb@gmail.com>
2016-05-19 02:03:14 +0300

    Service unit testing with Jasmine (not working yet)

--- tests/jasmine/services/test_users.js
+++ tests/jasmine/services/test_users.js
@@ -1,5 +1,25 @@
-describe('Just some dummy test', function() {
-  it('compares two primitives', function() {
-    expect(true).toEqual(true);
+describe('Users service', function () {
+  beforeEach(module('testApp'));
+
+  var $http, appUsers;
+
+  beforeEach(function(){
+    fixture.setBase('../../data');
+    this.result = fixture.load('users.json');
+  });
+
+  // load app services (app modules)
+  beforeEach(inject(function(_$httpBackend_, _appUsers_) {
+    $http = _$httpBackend_;
+    appUsers = _appUsers_;
+  }));
+
+  describe('receives users list', function() {
+    it('makes http request', function() {
+      $http.when('GET', '/data/users.json').respond(this.result);    
+      appUsers.getList().then(function(response){
+        expect(appUsers.data.list.length).toEqual(this.result.length);
+      });
+    });
   });
 });
