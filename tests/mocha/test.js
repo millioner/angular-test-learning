50339d5fa120ef417ad17f03f39a6354b3e53441
Millioner <millioner.bbb@gmail.com>
2016-05-20 10:41:05 +0300

    Mocha tests

--- tests/mocha/test.js
+++
@@ -1,8 +0,0 @@
-describe('Test', function() {
-  describe('of tests', function () {
-    it('just dummy test', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-  });
-});
